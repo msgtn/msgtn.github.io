0f3d0c3a5782c614eeeac74fdae4d7cbdeeaba21
Michael Suguitan <psychomugs@gmail.com>
2023-05-17 16:20:40 -0400

    Adding image/grad

--- pages/image.md
+++ pages/image.md
@@ -1,5 +1,5 @@
 ---
-layout: page
+layout: category
 title: IMAGE
 category: image
 permalink: /image
--- scratch.py
+++ scratch.py
@@ -1,15 +1,17 @@
 #%%
 links = '''
 
-assets/mpi_imgs/pictures/MJS_019_20230222_2990.jpg
+assets/photos/grad/DSCF6622.jpg
 
-assets/mpi_imgs/pictures/MJS_025_20230222_3117.jpg
+assets/photos/grad/FXP23897.jpg
 
-assets/mpi_imgs/pictures/MJS_023_20230222_2975.jpg
+assets/photos/grad/FXT24422-2.jpg
 
-assets/mpi_imgs/pictures/MJS_010_20230221_1602.jpg
+assets/photos/grad/FXT27030.jpg
 
+assets/photos/grad/FXT27802.jpg
 
+assets/photos/grad/FXP20023.jpg
 '''
 
 tag = ''
@@ -19,7 +21,7 @@ for link in links.split():
     # if len(link)<: continue
     _link = link.split()[-1]
     tag += (f'''
-    <a data-lightbox="blossom" href="{_link}" width="95%">
+    <a data-lightbox="img" href="{_link}" width="95%">
         <img src="{_link}" width="95%">
     </a>\n
     ''')
